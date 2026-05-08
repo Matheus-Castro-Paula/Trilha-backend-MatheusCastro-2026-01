8fe7a6eb422690e124ce00a1a06d8bd34c956e29
Matheus de Castro Paula <castro.matheus2k20@gmail.com>
2026-05-07 23:33:33 -0300

    feat: implementa CRUD de usuários

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 const AuthController = require("../controllers/authController");
 
-/**
- * Rotas públicas de autenticação de usuários.
- */
+//Rotas públicas de autenticação de usuários (/auth).
 router.post("/", AuthController.register);
 router.post("/login", AuthController.login);
 router.post("/forgot-password", AuthController.forgotPassword);
